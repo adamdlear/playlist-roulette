a9026080b8e08b0f862ea47701e8c599d71975bd
Adam Lear <adamlear12@gmail.com>
2025-01-19 21:11:22 -0600

    add spotify profile id to token and session

--- packages/web/src/auth.ts
+++ packages/web/src/auth.ts
@@ -3,4 +3,14 @@ import Spotify from "next-auth/providers/spotify";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
     providers: [Spotify],
+    callbacks: {
+        jwt({ token, profile }) {
+            if (profile && profile.id) token.profileId = profile.id;
+            return token;
+        },
+        session({ token, session }) {
+            session.user.profileId = token.profileId as string;
+            return session;
+        },
+    },
 });
