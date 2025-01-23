9eab0986b61c3e9fcfef2a34b94932696972831e
Adam Lear <adamlear12@gmail.com>
2025-01-22 22:10:49 -0600

    link connections table to ws functions

--- infra/api.ts
+++ infra/api.ts
@@ -1,3 +1,11 @@
+import { connectionsTable } from "./tables";
+
 export const wsApi = new sst.aws.ApiGatewayWebSocket("WebsocketApi");
-wsApi.route("$connect", "./packages/functions/src/connect.handler");
-wsApi.route("$disconnect", "./packages/functions/src/disconnect.handler");
+wsApi.route("$connect", {
+    link: [connectionsTable],
+    handler: "./packages/functions/src/connect.handler",
+});
+wsApi.route("$disconnect", {
+    link: [connectionsTable],
+    handler: "./packages/functions/src/disconnect.handler",
+});
