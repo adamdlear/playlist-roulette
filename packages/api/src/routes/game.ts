83f2edb1cf62228e29289fb6d7dd832ef8eb8f20
Adam Lear <adamlear12@gmail.com>
2025-06-08 11:05:49 -0500

    feat: add endpoint to start the game

--- packages/api/src/routes/game.ts
+++ packages/api/src/routes/game.ts
@@ -1,4 +1,8 @@
-import { DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb";
+import {
+	DynamoDBClient,
+	PutItemCommand,
+	UpdateItemCommand,
+} from "@aws-sdk/client-dynamodb";
 import { Context, Hono } from "hono";
 import { HTTPException } from "hono/http-exception";
 import { Resource } from "sst/resource";
@@ -18,7 +22,6 @@ game.post("/", async (c: Context) => {
 		Item: {
 			gameId: { S: gameId },
 			hostId: { S: hostId },
-			startTime: { S: Date.now().toString() },
 		},
 	});
 
@@ -29,6 +32,33 @@ game.post("/", async (c: Context) => {
 		console.error(error);
 		throw new HTTPException(400, {
 			message: `Error creating the job: ${error}`,
+		});
+	}
+});
+
+game.post("/start", async (c: Context) => {
+	const body = await c.req.json();
+
+	const { gameId } = body;
+
+	const updateGameCommand = new UpdateItemCommand({
+		TableName: Resource.Games.name,
+		Key: {
+			gameId: { S: gameId },
+		},
+		UpdateExpression: "SET Status = :status, StartTime = :startTime",
+		ExpressionAttributeValues: {
+			":status": { S: "IN_PROGRESS" },
+			":startTime": { N: Date.now().toString() },
+		},
+	});
+
+	try {
+		dynamodb.send(updateGameCommand);
+	} catch (error) {
+		console.error(error);
+		throw new HTTPException(400, {
+			message: `Error updating the job: ${error}`,
 		});
 	}
 });
