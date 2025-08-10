e6c6f42a994b54e4d5368f805c6fca89f46efaee
Adam Lear <adamlear12@gmail.com>
2025-08-09 21:01:34 -0500

    chore: add back gameCode

--- packages/web/src/components/game/game-card.tsx
+++ packages/web/src/components/game/game-card.tsx
@@ -1,14 +1,14 @@
 import { Card, CardTitle, CardDescription, CardHeader } from "../ui/card";
 
-// interface GameCardProps {
-// 	gameCode: string;
-// }
+interface GameCardProps {
+	gameCode: string;
+}
 
-export const GameCard = () => {
+export const GameCard = ({ gameCode }: GameCardProps) => {
 	return (
 		<Card className="w-[600px]">
 			<CardHeader className="text-center">
-				<CardTitle className="text-3xl">Guess Who</CardTitle>
+				<CardTitle className="text-3xl">Guess Who {gameCode}</CardTitle>
 				<CardDescription>
 					Select which player(s) have this song saved in a playlist
 				</CardDescription>
